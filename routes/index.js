0c6aa562778f4a335e91dd8c58fe2cec3ec2fbbf
klaus <oriokiklaus@gmail.com>
2023-12-04 17:48:43 +0300

    add route to get files endpoint

--- routes/index.js
+++ routes/index.js
@@ -18,16 +18,16 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 router.get('/users/me', UsersController.getMe);
 
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
-// router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id', FilesController.getShow);
 
-// router.get('/files', FilesController.getIndex);
+router.get('/files', FilesController.getIndex);
 
-// router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putPublish);
 
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
